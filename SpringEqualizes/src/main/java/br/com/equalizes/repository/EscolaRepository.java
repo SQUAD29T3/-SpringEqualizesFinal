7c576059c81484423414b9050e5466fb11beef9c
m40r1 <99940252+m40r1@users.noreply.github.com>
2022-11-10 02:13:19 +0000

    achar escola|empresa pelo cnpj

--- SpringEqualizes/src/main/java/br/com/equalizes/repository/EscolaRepository.java
+++ SpringEqualizes/src/main/java/br/com/equalizes/repository/EscolaRepository.java
@@ -1,20 +1,31 @@
 package br.com.equalizes.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import br.com.equalizes.model.Escola;
 
 public interface EscolaRepository extends JpaRepository<Escola, Long> {
 
 	// LISTA APENAS AS ESCOLAS ACEITAS
-	@Query(value = "select * from escola where status_cadastro = :status", nativeQuery = true)
-	public List<Escola> findByStatus(String status);
+	// @Query(value = "select * from escola where status_cadastro = :status",
+	// nativeQuery = true)
+	// public List<Escola> findByStatus(String status);
+
+	// Devia funcionar tbm
+	public List<Escola> findByStatusCadastro(String status);
 
 	// SELECT PARA REALIZAR O LOGIN
 	@Query(value = "select * from escola where email = :email and senha = :senha", nativeQuery = true)
 	public Escola Login(String email, String senha);
+
+	public Optional<Escola> findByCnpj(Long cnpj);
 
 }
--- SpringEqualizes/src/main/java/br/com/equalizes/repository/PedidoRepository.java
+++ SpringEqualizes/src/main/java/br/com/equalizes/repository/PedidoRepository.java
@@ -7,16 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 
 import br.com.equalizes.model.Pedido;
 
+//TODO simplificar?
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-	
+
 	// SELECIONA OS PEDIDOS EM ABERTO
 	@Query(value = "select * from pedido where status_pedido = 'em aberto' and escola_id = :escola", nativeQuery = true)
 	public List<Pedido> buscarEmAberto(Long escola);
-	
+
 	// SELECIONA OS PEDIDOS EM ANDAMENTO
 	@Query(value = "select * from pedido where status_pedido like 'em andamento' and escola_id = :escola", nativeQuery = true)
 	public List<Pedido> buscarEmAndamento(Long escola);
-	
+
 	// SELECIONA OS PEDIDOS CONCLUÍDOS
 	@Query(value = "select * from pedido where status_pedido like 'concluido' and escola_id = :escola", nativeQuery = true)
 	public List<Pedido> buscarConcluido(Long escola);
