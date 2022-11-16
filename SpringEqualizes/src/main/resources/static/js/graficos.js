9c5f51f0b53ad7735667555b24df35a3f006884e
giovanesouza <developergiovanesouza@gmail.com>
2022-11-15 21:48:53 -0300

    Ajustes no layout: Empresas cadastradas / empresas aprovadas + Escolas cadastradas / aprovadas + Solicitações de contato + Doações + Js para o código dos gráficos

--- SpringEqualizes/src/main/resources/static/js/graficos.js
+++ SpringEqualizes/src/main/resources/static/js/graficos.js
@@ -1,21 +1,60 @@
-/*      google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
-        ]);
 
-        var options = {
-          title: 'My Daily Activities',
-          is3D: true,
-        };
+/* TOTAL DE CADASTROS ATIVOS - ESCOLAS / EMPRESAS */
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-        chart.draw(data, options);
-      }
-      */
+const totalEsc = document.querySelector('#totalEscolas').value;
+const escola = parseInt(totalEsc);
+
+const totalEmp = document.querySelector('#totalEmpresas').value;
+const empresa = parseInt(totalEmp);
+
+
+/* TOTAL DE PEDIDOS - ESCOLAS / EMPRESAS */
+ 
+const abertoTotal = document.querySelector('#totalEmAberto').value;
+const aberto = parseInt(abertoTotal);
+
+const andamentoTotal = document.querySelector('#totalEmAndamento').value;
+const andamento = parseInt(andamentoTotal);
+
+const concluidoTotal = document.querySelector('#totalConcluido').value;
+const concluido = parseInt(concluidoTotal)
+
+
+
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart,);
+
+function drawChart() {
+
+	var data = google.visualization.arrayToDataTable([
+		['Tipo de parceria', 'Total'],
+		['Escolas', escola],
+		['Empresas', empresa]
+
+	]);
+
+	var dados = google.visualization.arrayToDataTable([
+		['Pedidos', 'Total'],
+		['Em aberto', aberto],
+		['Em andamento', andamento],
+		['Concluídos', concluido]
+	]);
+
+	var options = {
+		title: 'INSTITUIÇÕES PARCEIRAS - CADASTROS REALIZADOS'
+
+	};
+
+	var option = {
+		title: 'PEDIDOS REALIZADOS'
+	};
+
+// GRÁFICO QUE REPRESENTA AS INSTITUIÇÕES PARCEIRAS
+	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+	chart.draw(data, options);
+
+// GRÁFICO QUE REPRESENTA OS PEDIDOS
+	var newChart = new google.visualization.PieChart(document.getElementById('piechartPedidos'));
+	newChart.draw(dados, option);
+
+}
