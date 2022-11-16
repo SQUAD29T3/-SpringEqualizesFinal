2bc0d143e635da0d3351edcac67dc54f6502c593
m40r1 <99940252+m40r1@users.noreply.github.com>
2022-11-16 01:46:20 +0000

    ajustes para o login

--- SpringEqualizes/src/main/java/br/com/equalizes/repository/EscolaRepository.java
+++ SpringEqualizes/src/main/java/br/com/equalizes/repository/EscolaRepository.java
@@ -1,38 +1,31 @@
 package br.com.equalizes.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import br.com.equalizes.model.Escola;
 
 public interface EscolaRepository extends JpaRepository<Escola, Long> {
 
 	// LISTA APENAS AS ESCOLAS ACEITAS
-	@Query(value = "select * from escola where status_cadastro = :status",
-	nativeQuery = true)
-	public List<Escola> findByStatus(String status);
+	// @Query(value = "select * from escola where status_cadastro = :status",
+	// nativeQuery = true)
+	// public List<Escola> findByStatus(String status);
 
+	// Devia funcionar tbm
+	public List<Escola> findByStatusCadastro(String status);
 
 	// SELECT PARA REALIZAR O LOGIN
-	@Query(value = "select * from escola where email = :email and senha = :senha", nativeQuery = true)
-	public Escola Login(String email, String senha);
-	
-	
-	// VERIFICA SE HÁ ALGUM CADASTRO COM O CNPJ INSERIDO
-	@Query(value="select * from escola where cnpj = :cnpj", nativeQuery = true)
-	public Escola findByCnpj(String cnpj);
-	
-	
-	// VERIFICA QUANTOS CADASTROS HÁ NO BD
-	@Query(value="Select count(*) id, cnpj, nome_escola, qt_alunos, turnos, diretor, vice_diretor, coordenador, secretaria, "
-			+ "  rua, complemento, numero, bairro, cidade, uf, cep, data_cadastro,"
-			+ " data_resposta, status_cadastro, status_perfil, telefone, email, senha from escola;", nativeQuery = true)
-	public Escola totalPerfisAtivos();
+	@Query(value = "select * from escola where email = :email", nativeQuery = true)
+	public Escola Login(String email);
 
-	
-	
-	
+	public Optional<Escola> findByCnpj(Long cnpj);
 
 }
